2cb4dd5b769ee5552270b1a33f2d7e36c320a83f
Mark Erdmann <mark@crowdflower.com>
2010-09-11 21:54:43 -0700

    redis

--- maven.rb
+++ maven.rb
@@ -4,6 +4,12 @@ Bundler.setup
 
 require 'sinatra'
 
+configure do
+  require 'redis'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 get '/' do
   "hello bitches"
 end
