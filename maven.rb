d4ce5835870e3015f01c1217567fe48f6201eea2
Yin Yin Wu <yinyinwu5@yahoo.com>
2010-09-12 01:38:53 -0700

    initial layout and protovis graphs

--- maven.rb
+++ maven.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
 require 'bundler'
+require 'erb'
+
 Bundler.setup
 
 require 'sinatra'
 
 get '/' do
-  "hello bitches"
+  erb :home
 end
+
+#get '/friend/:userid' do
+ #   "#{params[:name]}"
+#end
